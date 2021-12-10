86e9385d553a6f3ec11105f9a21cc90428c8b285
PaulHuebler <paul.huebler@student.tu-freiberg.de>
2021-12-10 16:37:25 +0100

    Übertragen von Type und Timestamp des Entrys in die Datenbank - > TODO: GPS

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ import {
   doc,
   getDoc,
   updateDoc,
+  Timestamp,
 } from "firebase/firestore";
 import { getDatabase, ref, onValue, set } from "firebase/database";
 
@@ -76,6 +77,127 @@ export default function App() {
   const [user, setUser] = useState(null);
   const [statConfig, setStatConfig] = useState(null);
 
+  const handleLogin = async () => {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId:
+          "31827165734-rdbihglcac1juesc6fkjd4bgp1c1oj2s.apps.googleusercontent.com",
+        scopes: ["profile", "email"],
+      });
+
+      if (result.type === "success") {
+        try {
+          await refreshUser(result.user);
+          /* setUser({
+          username: result.user.name
+        }); */
+        } catch (e) {
+          console.log("Error:", e);
+        }
+
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  };
+
+  const refreshUser = async (user) => {
+    //Referenz zu Nutzerdokument, durch Google-Username identifiziert
+    const docRef = doc(firestore, "users", user.name);
+    //Snapshot von diesem Dokument zum Lesen
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      setUser({
+        username: docSnap.data().username,
+        email: docSnap.data().email,
+        photoUrl: docSnap.data().photoUrl,
+        joint_counter: docSnap.data().joint_counter,
+        bong_counter: docSnap.data().bong_counter,
+        vape_counter: docSnap.data().vape_counter,
+        member_since: docSnap.data().member_since,
+        main_counter:
+          docSnap.data().joint_counter +
+          docSnap.data().bong_counter +
+          docSnap.data().vape_counter,
+      });
+      setStatConfig({
+        joint: docSnap.data().show_joint,
+        bong: docSnap.data().show_bong,
+        vape: docSnap.data().show_vape,
+      });
+    } else {
+      //nutzer loggt sich erstmalig ein -> dokument erstellen
+      try {
+        await setDoc(doc(firestore, "users", user.name), {
+          username: user.name,
+          email: user.email,
+          photoUrl: user.photoUrl,
+          joint_counter: 0,
+          bong_counter: 0,
+          vape_counter: 0,
+          show_joint: true,
+          show_bong: true,
+          show_vape: true,
+          member_since: new Date().toISOString().slice(0, 10),
+          main_counter: 0,
+        });
+        console.log(user.photoUrl);
+        const docSnap = await getDoc(doc(firestore, "users", user.name));
+        if (docSnap.exists()) {
+          setUser({
+            username: docSnap.data().username,
+            email: docSnap.data().email,
+            photoUrl: docSnap.data().photoUrl,
+            joint_counter: docSnap.data().joint_counter,
+            bong_counter: docSnap.data().bong_counter,
+            vape_counter: docSnap.data().vape_counter,
+            member_since: docSnap.data().member_since,
+            main_counter:
+              docSnap.data().joint_counter +
+              docSnap.data().bong_counter +
+              docSnap.data().vape_counter,
+          });
+          setStatConfig({
+            joint: docSnap.data().show_joint,
+            bong: docSnap.data().show_bong,
+            vape: docSnap.data().show_vape,
+          });
+        }
+      } catch (e) {
+        console.log("Error:", e);
+      }
+    }
+  };
+
+  const handleLogOut = async () => {
+    try {
+      await Google.logOutAsync({
+        androidClientId:
+          "31827165734-rdbihglcac1juesc6fkjd4bgp1c1oj2s.apps.googleusercontent.com",
+        scopes: ["profile", "email"],
+      });
+    } catch (e) {
+      console.log("Error:", e);
+    }
+    setUser(null);
+  };
+
+  function writeDb(type) {
+    set(
+      ref(db, "users/" + user.username + "/entry_" + (user.main_counter + 1)),
+      {
+        type: type,
+        timestamp: Timestamp.now().seconds,
+        latitude: "TODO",
+        longitude: "TODO",
+      }
+    );
+  }
+
   const toggleCounter = async (index) => {
     //Referenz zu Nutzerdokument, durch Google-Username identifiziert
     const docRef = doc(firestore, "users", user.username);
@@ -86,16 +208,19 @@ export default function App() {
         await updateDoc(docRef, {
           joint_counter: docSnap.data().joint_counter + 1,
         });
+        writeDb("joint");
         break;
       case 2:
         await updateDoc(docRef, {
           bong_counter: docSnap.data().bong_counter + 1,
         });
+        writeDb("bong");
         break;
       case 3:
         await updateDoc(docRef, {
           vape_counter: docSnap.data().vape_counter + 1,
         });
+        writeDb("vape");
         break;
     }
 
